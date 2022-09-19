d068705c6dc9f0f45ffcfcf38dbca3cb3dd47a52
pullerize <pullerize@gmail.com>
2022-09-19 11:19:37 +0300

    new info add

--- Program.cs
+++ Program.cs
@@ -1,79 +1,152 @@
-﻿/* Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵)    /   2, 4 -> 16 
+﻿/* Задача 1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2 
 
-int Find(int number_a, int number_b)
+int [] Create_random_array(int size, int min_value, int max_value) // size = 4
 {
-    int result = 1;
-    for (int i = 1; i <= number_b; i++)
+    int[] new_array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        result = result * number_a;
+        new_array[i] = new Random().Next(min_value, max_value + 1);
     }
+    return new_array;
+}
+
+// void Show_array(int[] array) // Для проверки
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");           
+//     }
+//     Console.WriteLine();
+// }
+
+
+void Сhecking_even_values (int [] array)
+{
     
-    return result;
-}
-
-Console.Write("Введите число A: ");
-int number_a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int number_b = Convert.ToInt32(Console.ReadLine());
-
-Console.Write($"Возведенние из числа {number_a} в число {number_b} равняется : {Find(number_a, number_b)}");
-*/
-
-/* Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-452 -> 11   /   82 -> 10    /   9012 -> 12 
-
-
-int Sum_num(int num)
-{
-    int counter = Convert.ToString(num).Length; // 3
-    int advance = 0;
-    int result = 0;
-
-    for (int current = 0; current < counter; current++)
+    for (int i = 0; i < array.Length; i++)
     {
-        advance = num - num % 10;
-        result = result + (num - advance);
-        num = num / 10;
+         if (array[i] % 2 == 0)
+         {
+            Console.Write($"{array[i]} является четным значением. // ");
+         }
     }
-    return result;
 }
 
-Console.Write("Введите число : ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное трехзначное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное трехзначное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Сумма цифр из числа {num} равна: {Sum_num(num)}");
-*/
+int[] my_array = Create_random_array(size, min, max);
+// Show_array(my_array); // Для проверки
+Сhecking_even_values(my_array);
+ */
 
-/* Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]  /   6, 1, 33 -> [6, 1, 33] 
 
+ /* Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+ [3, 7, 23, 12] -> 19   /   [-4, -6, 89, 6] -> 0 
 
-static void Show_array(string[] args)
+ int [] Create_random_array(int size, int min_value, int max_value) 
 {
-
-    Console.Write("Введите количество элементов массива: ");
-
-    int element_count = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Вывод массива: ");
-
-    int [] my_array = new int [element_count];
-
-    for (int i = 0; i < my_array.Length; i++)
-    { 
-        Console.Write($"Введите значение массива под индексом {i}: ");
-        my_array[i] = Convert.ToInt32(Console.ReadLine());
-    }
-
-    Console.Write($"Значения массива в {element_count} елементах: ");
-
-    for (int i = 0; i < my_array.Length; i++)
+    int[] new_array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        Console.Write($"[{my_array[i]}] ");
+        new_array[i] = new Random().Next(min_value, max_value + 1);
     }
-   
+    return new_array;
 }
 
-Show_array(args);
-*/
+void Show_array(int[] array) // Для проверки
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");           
+    }
+    Console.WriteLine();
+}
+
+void Сhecking_even_values (int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i +2)
+    {         
+        sum += array[i]; 
+    }    
+    Console.WriteLine($"Сумма элементов стоящих на нечетных позициях равна: {sum}"); 
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] my_array = Create_random_array(size, min, max);
+Show_array(my_array); // Для проверки
+Сhecking_even_values(my_array);*/
+
+/* Задача 3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.*/
+
+
+double[] Array(int size)
+{
+    double[] array = new double[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble();
+        array[i] = Math.Round(array[i], 3);
+    }
+
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    int size = array.Length;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(i == 0) Console.Write($"[{array[i]}, ");
+        if((i > 0) && (i < size - 1)) Console.Write($"{array[i]}, ");
+        if(i == size - 1) Console.WriteLine($"{array[i]}]");
+    }
+
+    Console.WriteLine();
+}
+
+double Min(double[] array)
+{
+    int size = array.Length;
+    double min = array[0];
+    for(int i = 1; i < size; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+
+    return min;
+}
+
+double Max(double[] array)
+{
+    int size = array.Length;
+    double max = array[0];
+    for(int i = 1; i < size; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+
+    return max;
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+double[] array = Array(sizeArray);
+ShowArray(array);
+
+Console.WriteLine($"Минимальный элемент: {Min(array)}. \nМаксимальный элемент: {Max(array)}");
+Console.WriteLine($"Разница равна: {Max(array) - Min(array)}"); // [3 7 22 2 78] -> 76
